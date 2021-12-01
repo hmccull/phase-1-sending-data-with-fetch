ea86c6d1931906e842a82492b451f2683bbf5a32
Hannah McCullough <hmac091@gmail.com>
2021-11-30 19:48:56 -0700

    commit tree e3ec976bb6f5d20cc0fc8b1d387c8427c60a38f6

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    const userInput = {
+        name: userName,
+        email: userEmail
+    };
+
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        }, 
+        body: JSON.stringify(userInput),
+    })
+    .then(response => response.json())
+    .then(userInput => document.body.append(userInput.id))
+
+    .catch(function (error) {
+        alert("Input values were not posted correctly.");
+        document.body.append(error.message);
+    })
+}
+
+/*
+When writing the callback function for your catch(), expect to receive an object on error with a property, message, containing info about what went wrong. Write code to append this message to the DOM when catch() is called.
+
+An amazing feature of fetch() is that if you return it, other functions can tack on their own then() and catch() calls. For this lab, you will need to return the fetch() chain from our submitData function to ensure that the tests run correctly.
+*/
